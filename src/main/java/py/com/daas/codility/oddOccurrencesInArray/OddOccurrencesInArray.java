cde608a81053f9a236f8825f45f371c93161db05
daasalbion <mg908295@gmail.com>
2019-12-21 20:31:38 -0300

    odd occurrencesinarray added with 100%

--- src/main/java/py/com/daas/codility/oddOccurrencesInArray/OddOccurrencesInArray.java
+++ src/main/java/py/com/daas/codility/oddOccurrencesInArray/OddOccurrencesInArray.java
@@ -8,16 +8,10 @@ public class OddOccurrencesInArray {
         int middle = A.length/2;
         HashSet<Integer> set = new HashSet<>();
         set.add(A[middle]);
-        return doRecursiveSolution(set, A, 0, A.length - 1);
-    }
-
-    private static Integer doRecursiveSolution(HashSet<Integer> set, int[] A, int start, int ends) {
-        if ((ends - start) > 1) {
-            insertOrDelete(set, A[start]);
-            insertOrDelete(set, A[ends]);
-            return doRecursiveSolution(set, A, start+1, ends-1);
+        for (int i = 0; i < middle; i++) {
+            insertOrDelete(set, A[i]);
+            insertOrDelete(set, A[middle + i + 1]);
         }
-
         return set.iterator().next();
     }
 
