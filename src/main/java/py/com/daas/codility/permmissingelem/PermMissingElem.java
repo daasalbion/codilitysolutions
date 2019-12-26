16652809805a2c60d31e369c7ce7a6f8ebc7fcdd
Derlis Arguello <derlisarguello01@gmail.com>
2019-12-26 15:52:53 -0300

    PermMissingElem

--- src/main/java/py/com/daas/codility/permmissingelem/PermMissingElem.java
+++ src/main/java/py/com/daas/codility/permmissingelem/PermMissingElem.java
@@ -1,36 +1,36 @@
 package py.com.daas.codility.permmissingelem;
 
+import java.util.HashSet;
+
 /**
  * @author Ing. Derlis Argüello <derlisarguello01@gmail.com>
  */
 public class PermMissingElem {
 
     public static int solution(int[] A) {
-        int n = A.length;
-        return getFactorial(A, n);
+        int N = A.length;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            insertOrDelete(set, i + 1);
+            insertOrDelete(set, A[i]);
+        }
+        insertOrDelete(set, N + 1);
+        return set.iterator().next();
     }
 
-    private static int getFactorial(int[] A, int n) {
-        int fact0 = 1;
-        int fact1 = 1;
-        int nplusOne = n + 1;
-        for (int i = 0; i < n; i++) {
-            if (A[i] == 0) {
-                A[i] = 1;
-                nplusOne = 1;
-            }
-            fact0 = fact0 * A[i];
-            fact1 = fact1 * (i + 1);
+    public static void insertOrDelete(HashSet<Integer> set, Integer value) {
+        if (set.contains(value)) {
+            set.remove(value);
+        } else {
+            set.add(value);
         }
-        fact1 = fact1 * (nplusOne);
-        int r = fact1/fact0;
-        return r;
     }
 
     public static void main(String[] args) {
         //int[] A = {7, 5, 2, 1, 3, 6};
-        int[] A = {2, 3, 1, 5};
-        int sol = solution(A);
+        //int[] A   = {2, 3, 1, 5};
+        int[] A   = {1};
+        int   sol = solution(A);
         System.out.println("solution " + sol);
     }
 
