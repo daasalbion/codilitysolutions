420320c139476748b8d202ea7db9995cef728325
Derlis Arguello <derlisarguello01@gmail.com>
2019-12-27 10:35:56 -0300

    see

--- src/main/java/py/com/daas/codility/TapeEquilibrium/TapeEquilibrium.java
+++ src/main/java/py/com/daas/codility/TapeEquilibrium/TapeEquilibrium.java
@@ -6,25 +6,33 @@ package py.com.daas.codility.TapeEquilibrium;
 public class TapeEquilibrium {
 
     public static int solution(int[] A) {
-        int N = A.length;
-        int leftSum = 0;
-        int rigthSum = 0;
-        int difference = 0;
+        int   N    = A.length;//must have least two elements
+        int[] tmpA = new int[N - 1];
+        tmpA[tmpA.length-1] = A[N - 1];
+        int leftSum       = A[0];
+        int rigthSum      = tmpA[tmpA.length-1];
+        int difference    = 0;
         int minDifference = Integer.MAX_VALUE;
-        int j = 0;
+        int j             = 0;
         for (int i = 1; i < (N - 1); i++) {
-            j = (N-1)- i;
-            leftSum = (A[i] + A[i - 1]);
-            rigthSum = (A[j] + A[j + 1]);
+            j = (N - 1) - i;
+            leftSum += A[i];
+            rigthSum += A[j];
             if (i >= j) {
-                difference = Math.abs(leftSum - A[j+1]);
-                if (difference < minDifference) {
-                    minDifference = difference;
-                }
-            } else {
-                A[i] = leftSum;
-                A[j] = rigthSum;
+                minDifference = getDifference(minDifference, rigthSum, A[i - 1]);
+                minDifference = getDifference(minDifference, leftSum, tmpA[i]);
             }
+            A[i] = leftSum;
+            tmpA[j-1] = rigthSum;
+        }
+
+        return minDifference;
+    }
+
+    public static int getDifference(int minDifference, int a, int b) {
+        int difference = Math.abs(a - b);
+        if (difference < minDifference) {
+            minDifference = difference;
         }
         return minDifference;
     }
